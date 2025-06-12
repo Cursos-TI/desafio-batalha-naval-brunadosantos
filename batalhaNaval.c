e094c480a72b5cae280200c7ddece5a75591e127
brunadosantos <bluisa286@gmail.com>
2025-06-12 20:58:45 +0000

    desenvolvi o nivel mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,32 +1,108 @@
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define linha 10
-#define coluna 10
-#define tamanho 3
+// declaração de constantes
+#define tamanho_tabuleiro 10
+#define tamanho_cone 5
+#define tamanho_cruz 5
+#define tamanho_octaedro 5
+#define tamanho_navio 3
 
-int main() {
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+// declaração de matriz
+int tabuleiro[tamanho_tabuleiro][tamanho_tabuleiro] = {0}; // define o tamanho da matriz em 10x10 e todos os indices recebem o valor 0.
+int tabuleiro_cone[tamanho_cone][tamanho_cone] = {0}; // define o tamanho da matriz em 5x5 e todos os indices recebem o valor 0.
+int tabuleiro_cruz[tamanho_cruz][tamanho_cruz] = {0}; // define o tamanho da matriz em 5x5 e todos os indices recebem o valor 0.
+char tabuleiro_octaedro[tamanho_octaedro][tamanho_octaedro] = {0}; // define o tamanho da matriz em 5x5 e todos os indices recebem o valor 0.
+
+// definição de funções
+void matrizCone(){
+    
+    // definição do cone
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = tamanho_cone / 2 - i; j <= tamanho_cone / 2 + i; j++)
+        {
+            if (j >= 0 && j < tamanho_cone)
+            {
+                tabuleiro_cone[i][j] = 5; // armazena os valores do cone
+            }
+        }
+    }
+}
+
+void matrizCruz(){
+
+    // declaração de variável para encontrar o meio do tabuleiro
+    int meio = tamanho_cruz / 2;
+
+    // definição da cruz
+    for (int i = 0; i < tamanho_cruz; i++)
+    {
+        tabuleiro_cruz[i][meio] = 7; // linha vertical    
+        tabuleiro_cruz[meio][i] = 7; // linha horizontal
+    }
+}
+
+void matrizOctaedro() {
+
+    // declaração de variável para encontrar o meio do tabuleiro
+    int centro = tamanho_octaedro / 2;
+    // definição do octaedro
+    for (int i = 0; i <= centro; i++)
+    {
+        for (int j = centro - i; j <= centro + i; j++)
+        {
+            tabuleiro_octaedro[i][j] = 9; // parte superior
+            tabuleiro_octaedro[tamanho_octaedro - i - 1][j] = 9; // parte inferior
+        }
+    }
+}
+
+void mesclarTabuleiro() {
+    // declaração de variáveis
+    int linha_cone = 0, coluna_cone = 0;
+    int linha_cruz = 4, coluna_cruz = 0;
+    int linha_octaedro = 0, coluna_octaedro = 5;
+    int valores;
+
+    // definição do cone
+    for (int i = 0; i < 3; i++) {
+        for (int j = tamanho_cone / 2 - i; j <= tamanho_cone / 2 + i; j++) {
+            if (tabuleiro_cone[i][j] != 0) {
+                tabuleiro[linha_cone + i][coluna_cone + j] = tabuleiro_cone[i][j];
+            }
+        }
+    }
+
+    // definição da cruz
+    for (int i = 0; i < tamanho_cruz; i++) {
+        for (int j = 0; j < tamanho_cruz; j++) {
+            if (tabuleiro_cruz[i][j] != 0) {
+                tabuleiro[linha_cruz + i][coluna_cruz + j] = tabuleiro_cruz[i][j];
+            }
+        }        
+    }
+
+    // definição do octaedro
+    for (int i = 0; i < tamanho_octaedro; i++) {
+        for (int j = 0; j < tamanho_octaedro; j++) {
+            tabuleiro[linha_octaedro + i][coluna_octaedro + j] = tabuleiro_octaedro[i][j];
+        }
+    }
+
+}
+
+void exibirTabuleiro(){
+    // declaração de vetor
+    char cabecalho_horizontal[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int cabecalho_vertical[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
     // Declaração de variáveis
-    int linhaHorizontal = 8, linhaVertical = 1;
+    int linhaHorizontal = 8, linhaVertical = 4;
     int colunaHorizontal = 0, colunaVertical = 9;
-    int linhaPrincipal = 2, linhaSecundaria = 2;
-    int colunaPrincipal = 2, colunaSecundaria = 7;
+    int linhaPrincipal = 7, linhaSecundaria = 3;
+    int colunaPrincipal = 7, colunaSecundaria = 6;
     int soma;
-
-    // Declaração da matriz
-    int tabuleiro[linha][coluna] = {0};
-
-    // Declaração do vetor
-    char cabecalhoHorizontal[coluna] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int cabecalhoVertical[linha] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     // Validação de erros
     if ((linhaHorizontal == colunaPrincipal) || (linhaVertical == colunaSecundaria) || (linhaHorizontal == linhaVertical))
@@ -34,12 +110,12 @@ int main() {
         // Validação de colisão
         // linhas e colunas não podem se sobrepor umas as outras
         printf("Houve uma colisão! Tente novamente.");
-    } else if ((linhaVertical + tamanho > 10) || (linhaHorizontal > 10) || (colunaVertical > 10) || (colunaHorizontal + tamanho > 10))
+    } else if ((linhaVertical + tamanho_navio > 10) || (linhaHorizontal > 10) || (colunaVertical > 10) || (colunaHorizontal + tamanho_navio > 10))
     { 
         // Validação de limite do tabuleiro horizontal e vertical
         // As variáveis linhas e colunas não podem ultrapassar 10 que é o tamanho limite do tabuleiro
         printf("O navio não cabe no tabuleiro! Tente novamente.");
-    } else if((linhaPrincipal + tamanho > 10) || (linhaSecundaria + tamanho > 10) || (colunaPrincipal + tamanho > 10) || (colunaSecundaria < 2))
+    } else if((linhaPrincipal + tamanho_navio > 10) || (linhaSecundaria + tamanho_navio > 10) || (colunaPrincipal + tamanho_navio > 10) || (colunaSecundaria < 2))
     { 
         // Validação de limite do tabuleiro diagonal principal e secundária
         // As variáveis linhas e colunas não podem ultrapassar 10 que é o tamanho limite do tabuleiro
@@ -57,28 +133,28 @@ int main() {
 
         // exibição das coordenadas horizontal
         printf("Coordenada horizontal: ");
-        for (int i = 0; i < tamanho; i++)
+        for (int i = 0; i < tamanho_navio; i++)
         {   
             // exibe a coordenada do navio e adiciona 1 ao valor da coluna
-            printf("(%d, %c) ", cabecalhoVertical[linhaHorizontal], cabecalhoHorizontal[colunaHorizontal + i]);
+            printf("(%d, %c) ", cabecalho_vertical[linhaHorizontal], cabecalho_horizontal[colunaHorizontal + i]);
         }
         printf("\n"); // separa as coordenadas para melhor visualização e organização no terminal
 
         // exibição das coordenadas vertical 
         printf("Coordenada vertical: ");
-        for (int i = 0; i < tamanho; i++)
+        for (int i = 0; i < tamanho_navio; i++)
         {   
             // exibe a coordenada do navio e adiciona 1 ao valor da linha
-            printf("(%d, %c) ", cabecalhoVertical[linhaVertical + i], cabecalhoHorizontal[colunaVertical]);
+            printf("(%d, %c) ", cabecalho_vertical[linhaVertical + i], cabecalho_horizontal[colunaVertical]);
         }
         printf("\n"); // separa as coordenadas e banner para melhor visualização e organização no terminal
 
         // exibição das coordenadas diagonal superios  
         printf("Coordenada diagonal superior: ");
-        for (int i = 0; i < tamanho; i++)
+        for (int i = 0; i < tamanho_navio; i++)
         {   
             // exibe a coordenada do navio na diagonal e adiciona 1 ao valor da linha e coluna
-            printf("(%d, %c) ", cabecalhoVertical[linhaPrincipal + i], cabecalhoHorizontal[colunaPrincipal + i]);
+            printf("(%d, %c) ", cabecalho_vertical[linhaPrincipal + i], cabecalho_horizontal[colunaPrincipal + i]);
         }
         printf("\n"); // separa as coordenadas para melhor visualização e organização no terminal
 
@@ -87,13 +163,13 @@ int main() {
         soma = linhaSecundaria + colunaSecundaria; // calcula os valores de linha e coluna para verificar o se resultado é igual a 9  
         if (soma == 9) // se resultado igual a nove executa o código, se não retorna erro.
         {
-            for (int i = 0; i < tamanho; i++)
+            for (int i = 0; i < tamanho_navio; i++)
             {
 
                 // define o posicionamento do navio na diagonal no tabuleiro
-                tabuleiro[linhaSecundaria + i][colunaSecundaria - i] = tamanho;
+                tabuleiro[linhaSecundaria + i][colunaSecundaria - i] = tamanho_navio;
                 // exibe a coordenada do navio na diagonal e adiciona 1 ao valor da linha e tira 1 da coluna
-                printf("(%d, %c) ", cabecalhoVertical[linhaSecundaria + i], cabecalhoHorizontal[colunaSecundaria - i]);
+                printf("(%d, %c) ", cabecalho_vertical[linhaSecundaria + i], cabecalho_horizontal[colunaSecundaria - i]);
             }
         } else {
             printf("Houve um erro! Tente novamente."); // se a condição for falsa retorna esse erro.
@@ -102,7 +178,7 @@ int main() {
 
         // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
         // posicionamento dos navios
-        for (int i = 0; i < tamanho; i++)
+        for (int i = 0; i < tamanho_navio; i++)
         {   
             //define a posição e armazena o número 3 para demarcar a localização dos navios
             /* Exemplo:
@@ -113,9 +189,9 @@ int main() {
                 É o mesmo que digitar:
                 tabuleiro[1][5 + i] = 3; 
             */
-            tabuleiro[linhaHorizontal][colunaHorizontal + i] = tamanho; // define o posicionamento do navio na horizontal e adiciona 1 na coluna
-            tabuleiro[linhaVertical + i][colunaVertical] = tamanho; // define o posicionamento do navio na vertical e adiciona 1 na linha
-            tabuleiro[linhaPrincipal + i][colunaPrincipal + i] = tamanho; // define o posicionamento do navio na diagonal no tabuleiro e adiciona 1 na linha e coluna
+            tabuleiro[linhaHorizontal][colunaHorizontal + i] = tamanho_navio; // define o posicionamento do navio na horizontal e adiciona 1 na coluna
+            tabuleiro[linhaVertical + i][colunaVertical] = tamanho_navio; // define o posicionamento do navio na vertical e adiciona 1 na linha
+            tabuleiro[linhaPrincipal + i][colunaPrincipal + i] = tamanho_navio; // define o posicionamento do navio na diagonal no tabuleiro e adiciona 1 na linha e coluna
         }
 
         // banner de exibição do tabuleiro
@@ -123,48 +199,46 @@ int main() {
         printf("------------ TABULEIRO ------------\n");
         printf("===================================\n");
         printf("\n"); // separa banner e cabeçalho para melhor visualização e organização no terminal
-
-        /* imprime o cabeçalho seguinte maneira:
-
-                |A| |B| |C| => horizontal
-            |0|  0   0   0
-            |1|  0   0   0
-            |2|  0   0   0
-             ^
-            Vertical
-        */
-
-        // exibição do cabeçalho horizontal 
-        printf("   "); // espaçamento para organizar a exibição do cabeçalho no terminal
-        // executa a repetição até que x se torne maior que 10 
-        for (int x = 0; x < 10; x++)
-        {   
-            // armazena o valor atual de x e exibe as letras correspondentes a posição no vetor
-            printf("|%c|", cabecalhoHorizontal[x]); 
-        }
-        printf("\n"); // evita que o cabeçalho e a primeira linha fiquem no mesmo espaço
-
-        // exibição do tabuleiro com a posição dos navios
-        for (int i = 0; i < linha; i++)
+    
+        // adiciona o cabeçalho horizontal
+        printf("   "); // cria um espaçamento para alinhar o cabeçalho a matriz
+        for (int x = 0; x < tamanho_tabuleiro; x++)
         {
-            // exibição do cabeçalho vertical
-            // armazena o valor atual de i e exibe os números correspondentes a posição no vetor
-            printf("|%d| ", cabecalhoVertical[i]); 
-            for (int j = 0; j < coluna; j++)
-            {
-                // imprime 0 até que i seja maior que 10 e j seja maior que 10 formando o tabuleiro
-                printf("%d  ", tabuleiro[i][j]);  
+            printf("|%c|", cabecalho_horizontal[x]); // exibe o cabeçalho horizontal com caracteres |A|, |B|, |C|...
+        }
+        printf("\n");
+
+        // exibição da matriz principal
+        for (int i = 0; i < tamanho_tabuleiro; i++)
+        {   
+            printf("|%d| ", cabecalho_vertical[i]); // exibe o cabeçalho vertical com números |1|, |2|, |3|...
+            for (int j = 0; j < tamanho_tabuleiro; j++)
+            {   
+                printf("%d  ", tabuleiro[i][j]); // imprime os valores da linha e coluna da matriz tabuleiro
             }
-            printf("\n"); // separa as linhas para melhor visualização e organização do tabuleiro
+            printf("\n");
         }
     }
     printf("\n"); // separa as linhas para melhor visualização e organização do tabuleiro
+}
+
+int main() {
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
     // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
+    // chamada de função
+    matrizCone();
+    matrizCruz();
+    matrizOctaedro();
+    mesclarTabuleiro();
+    exibirTabuleiro();
+
+    return 0;
+}
+
+// Exemplos de exibição das habilidades:
     // Exemplo para habilidade em cone:
     // 0 0 1 0 0
     // 0 1 1 1 0
@@ -179,6 +253,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
